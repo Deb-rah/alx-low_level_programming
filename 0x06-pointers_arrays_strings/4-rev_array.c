064c979cb2754de1c9992f6e422efb91d09bc907
Deb-rah <deborahbamigboye@gmail.com>
2022-07-14 17:31:45 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,21 +2,18 @@
 
 /**
  * reverse_array -> reversing an array
- * @a: array a
- * @n: an element of an array
+ * @a: pointer
+ * @n: array length
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
-
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < i / 2; k++)
+	int i = 0, half;
+	
+	for (half = n / 2; half > 0; half--, i++)
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		a[n - i - 1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n - i - 1] = a[n - i - 1] - a[i];
 	}
 }
