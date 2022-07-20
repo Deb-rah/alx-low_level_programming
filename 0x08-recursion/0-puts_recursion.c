2b31cc7ffc82ef848c53de11f009fdf6b1b9a814
Deb-rah <deborahbamigboye@gmail.com>
2022-07-20 10:07:13 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,17 @@
 /**
  * _puts_recursion -> a function that prints a string
  * @s: string parameter
+ * Return: No
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
 }
