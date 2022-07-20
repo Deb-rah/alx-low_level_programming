a1034f1c4acd2a57d3cb629d702fb10ece4e6776
Deb-rah <deborahbamigboye@gmail.com>
2022-07-20 12:14:57 -0700

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,7 +8,7 @@ int _strlen_recursion(char *s);
  * @s: string to reverse
  * Return: 1 if it is, 0 if it's not
  */
-int is_palindromr(char *s)
+int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
